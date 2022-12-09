4f140a6f309c43b940522d6300e8553ca506c690
Ahmed Fouad <50051208+AhmedMohamed1432@users.noreply.github.com>
2022-12-09 20:24:27 +0200

    GUI frame done

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,4 +1,60 @@
 package view;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+
+    private JPanel MainPanel;
+    private JPanel SecondaryPanel;
+    private JPanel InitialPanel;
+    private JLabel InvoiceLabel;
+    private JButton createInvoiceBtn;
+    private JButton deleteInvoiceBtn;
+    private JTable invoicesTable;
+    private JLabel invNumber;
+    private JLabel invNumberVal;
+    private JScrollPane Scrollpanel;
+    private JLabel invoiceDate;
+    private JLabel custName;
+    private JTextField customerNameTextField;
+    private JTextField dateTextfiel;
+    private JLabel invoiceToatal;
+    private JLabel InvoiceItems;
+    private JTable lineTable;
+    private JButton saveBtn;
+    private JLabel intotalval;
+    private JButton canselBtn;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem loadMenuItem;
+    private JMenuItem saveMenuItem;
+
+    public MainFrame(String title) {
+        super(title);
+        //MenuBar
+        menuBar= new JMenuBar();
+        loadMenuItem= new JMenuItem("Load");
+        saveMenuItem= new JMenuItem("Save");
+        fileMenu= new JMenu("File");
+        fileMenu.add(loadMenuItem);
+        fileMenu.add(saveMenuItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+
+
+
+        this.setSize(1000, 700);
+        this.setLocation(400, 150);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setContentPane(InitialPanel);
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new MainFrame("Invoice Generator");
+        frame.setVisible(true);
+    }
+
+
 }
