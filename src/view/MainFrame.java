5c510ec6db55dad5ecd9f06b4e68420e06e7d8df
Ahmed Fouad <50051208+AhmedMohamed1432@users.noreply.github.com>
2022-12-09 22:43:51 +0200

    Date to string in invoiceHeader

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.Actions;
 import model.InvoiceHeader;
 import model.InvoiceTableModel;
 
@@ -33,13 +34,29 @@ public class MainFrame extends JFrame {
     private JMenuItem loadMenuItem;
     private JMenuItem saveMenuItem;
 
+
     private InvoiceTableModel invoiceTableModel;
     private ArrayList<InvoiceHeader> invoices;
-    //private Cntrolller controller = new controller (this);
-
+    private Actions controller = new Actions();
+    public ArrayList<InvoiceHeader> getInvoices() {
+        if (invoices == null) invoices = new ArrayList<>();
+        return invoices;
+    }
     public MainFrame(String title) {
         super(title);
         //MenuBar
+        this.setupMenuBar();
+        saveBtn.addActionListener(controller);
+        createInvoiceBtn.addActionListener(controller);
+        deleteInvoiceBtn.addActionListener(controller);
+        canselBtn.addActionListener(controller);
+        this.setSize(1000, 700);
+        this.setLocation(400, 150);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setContentPane(InitialPanel);
+
+    }
+    private void setupMenuBar(){
         menuBar= new JMenuBar();
         loadMenuItem= new JMenuItem("Load");
         saveMenuItem= new JMenuItem("Save");
@@ -48,14 +65,26 @@ public class MainFrame extends JFrame {
         fileMenu.add(saveMenuItem);
         menuBar.add(fileMenu);
         setJMenuBar(menuBar);
-
-        this.setSize(1000, 700);
-        this.setLocation(400, 150);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setContentPane(InitialPanel);
-
+        loadMenuItem.addActionListener(controller);
+        saveMenuItem.addActionListener(controller);
     }
 
+    public Actions getController(){return controller;}
+    public int getNextInvoiceNum() {
+        int num = 0;
+
+        for (InvoiceHeader invoice : getInvoices()) {
+            if (invoice.getInvoiceNumber() > num)
+                num = invoice.getInvoiceNumber();
+        }
+        return ++num;
+    }
+    public InvoiceTableModel getInvoicesTableModel() {
+        if (invoiceTableModel == null) {
+            invoiceTableModel = new InvoiceTableModel(getInvoices());
+        }
+        return invoiceTableModel;
+    }
     public static void main(String[] args) {
         JFrame frame = new MainFrame("Invoice Generator");
         frame.setVisible(true);
